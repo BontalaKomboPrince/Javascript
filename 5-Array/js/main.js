c1becd7e27cc98a784294e894feaf238baca112e
PrinceKombo <princekombo@gmail.com>
2018-01-24 02:58:46 +0100

    full repository with index and main fiels

--- 5-Array/js/main.js
+++ 5-Array/js/main.js
@@ -1,6 +1,9 @@
 
-
+//Exercice : addition
 let add = [1,2,3,4,5,6,7,8,9];
 
 console.log(add.reduce((a,b) => a+b));
 document.write(add.reduce((a,b) => a+b));
+
+let best = [" Smith ", " roberts ", " cruiz "];
+console.log( "the best one is"+ best[1]);
