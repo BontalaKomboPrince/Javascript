7497529b852e7d62e5848ac832563968a7bb92e5
PrinceKombo <princekombo@gmail.com>
2018-01-24 02:45:30 +0100

    reproduction du repository avec tous les exo

---
+++ 4-Functions/js/main.js
@@ -0,0 +1,49 @@
+// // Exercice : Calculs
+//
+// function sous(sous1,sous2){
+//   //function that can substract values
+//
+// return sous1-sous2;
+//
+// }
+// sous1= prompt("give me a number");
+// sous2= prompt("give me a number");
+//  document.write(sous(sous1,sous2));
+
+ //function operating divisions
+// function div(div1,div2){
+// return div1/div2;
+//
+// }
+// div1= prompt("give me a number");
+// div2= prompt("give me a number");
+//  document.write(div(div1,div2));
+//
+
+//function that makes multiplication
+// function mult(mult1,mult2){
+// return mult1*mult2;
+//
+// }
+// mult1= prompt("give me a number");
+// mult2= prompt("give me a number");
+//  document.write(mult(mult1,mult2));
+
+
+
+function per(per1,per2){
+return per1%per2;
+
+}
+per1= prompt("give me a number");
+per2= prompt("give me a number");
+ document.write(per(per1,per2));
+
+//Function that calculate the speed
+function speed(distance,time){
+return per1%per2;
+
+}
+per1= prompt("give me a number");
+per2= prompt("give me a number");
+ document.write(per(per1,per2));
---
+++ 5-Array/js/main.js
@@ -0,0 +1,6 @@
+
+
+let add = [1,2,3,4,5,6,7,8,9];
+
+console.log(add.reduce((a,b) => a+b));
+document.write(add.reduce((a,b) => a+b));
